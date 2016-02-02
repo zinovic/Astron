886b375f270d5b71dc5b3111a3dc9757ffe74230
Nicholas Zivkovic <niki.zivkovic@hotmail.com>
2016-02-02 20:26:19 +0000

    Stepper unexpected behavior
    Runs both motors at the same time until the diff is 2:1 then one is ran
    twice as fast as the other.

--- Stepper code.c
+++ Stepper code.c
@@ -41,17 +41,18 @@ unsigned int HalfStep[8]={0x1,0x3,0x2,0x6,0x4,0xC,0x8,0x9};
     // 0b00010000,0b00110000,0b00100000,0b01100000,0b01000000,0b11000000,0b10000000,0b10010000 //
     //       0x10,      0x30,      0x20,      0x60,      0x40,      0xC0,      0x80,      0x90 //
     /////////////////////////////////////////////////////////////////////////////////////////////
+
 // Pic functions
     void output_b (int foo){
         printf("Stp: %x\n", foo);
     }
 
     void delay_ms (int foo){
-        sleep (foo);
+        sleep (foo/10);
     }
 
     void delay_us (int foo){
-        sleep (foo);
+        sleep (foo/10000);
     }
 
 void twoStep(float lALT, float LAZ);
@@ -59,9 +60,9 @@ void twoStep(float lALT, float LAZ);
 int main(){
     while(1){
 
-        twoStep(10,3);
-        printf("Task completed!\n");
-        sleep(60);
+        twoStep(0.1,0.08);
+        printf("Task completed!\n\n\n\n");
+        sleep(1);
 /*        
     output_b(HalfStep[yStep&0x7]<<4|HalfStep[xStep&0x7]);   
     delay_ms(d);
@@ -74,43 +75,50 @@ int main(){
    
 void twoStep(float lALT, float lAZ){                        // ALT == y    AZ == x  in Rad and what the new cordinates need to be
         // We need to convert the Rad value to step equivilent
-    float y = (float)lALT*(float)(6144/(float)(pi/2));      // ALT we have 0 - pi/2 rad at 6144step  360/(5.625/64) = 4096 steps per 2pi; Gear ratio of 10:60 -> (60/4)/10 = 1.5 turns for 2/pi rad 4096*1.5 = 6144 steps for 90 deg or 2/pirad 
-    float x = (float)lAZ*(float)((6144*4)/(float)(2*pi));   // AZ  we have 0 - 2pi rad at (ALTsteps*4)       // at this step speed one revolution should take 24576ms or 24.6 seconds this means the longest if shoul take to a point the in the sky is 12.3 seconds
+    float yStepsTobe = (float)lALT/(float)((float)(pi/2)/6144);      // ALT we have 0 - pi/2 rad at 6144step  360/(5.625/64) = 4096 steps per 2pi; Gear ratio of 10:60 -> (60/4)/10 = 1.5 turns for 2/pi rad 4096*1.5 = 6144 steps for 90 deg or 2/pirad 
+    float xStepsTobe = (float)lAZ/(float)((float)(2*pi)/(6144*4));   // AZ  we have 0 - 2pi rad at (ALTsteps*4)       // at this step speed one revolution should take 24576ms or 24.6 seconds this means the longest if shoul take to a point the in the sky is 12.3 seconds
     
     // Lets convert the input radien and the current psition form step values to find the diffrence. 
     // max stp num for Alt is: 6144             6144/(pi/2) = 15645.56 So Alt Has 1
     // max stp num for AZ  is: 6144*4 = 24576
-/*    int xStepLeft = 6144/(pi/2)
-    int yStepLeft = */
-    
 
 
         // Now we need to find the step requiered to get there
-    int yLft = y - yStep;
-    int xLft = x - xStep;
-    
-    int yDir = 1;   if (lALT < 0)   yDir = -1;              //Assigning a value to know what direction to move the motors
-    int xDir = 1;   if (lAZ < 0)    xDir = -1;              //Using the nested if statment. 
+    int yToDo = yStepsTobe - yStep;
+    int xToDo = xStepsTobe - xStep;
+    float difs = yToDo / xToDo;
+    //printf("Mis Vals;                         dif:%f yToDo:%d xToDo:%d \n",difs,yToDo,xToDo);    
+   
+    int yDir = 1;   if (yToDo < 0)   yDir = -1;              //Assigning a value to know what direction to move the motors
+    int xDir = 1;   if (xToDo < 0)    xDir = -1;              //Using the nested if statment. 
 
     
         // Now we have if statments for the cases where one number is higher then the other and where they are equal
-    if (abs(yLft)>abs(xLft)){                               // IN this case Y is higher
-        int dif = yLft/xLft;
-        float mod = yLft%xLft;
-        int usDelay = mod * 1000;
-        printf("We made it this far!\n");
+    if (abs(yToDo)>abs(xToDo)){                               // IN this case Y is higher
+        
+        float dif = yToDo/xToDo;
+        //printf("The fuking diffrence! %4f\n",dif);
+        int intdif  = (int)dif;
+        float decpart = dif - intdif;
 
-        for (int *z = &xLft; *z > 0; *z--){                             // Here we itterate for the value of x as it lower 
+        int usDelay = decpart * 1000;
+        printf("We made it this far!\n");
+        printf("Mis Vals;                         decpart:%f usDelay:%d dif:%f yDir:%d xDir%d yToDo:%d xToDo:%d \n",decpart,usDelay,dif,yDir,xDir,yToDo,xToDo);    
+
+        for (int *z = &xToDo; *z > 0; *z--){                             // Here we itterate for the value of x as it lower 
             //delay_us(1000-usDelay);
         printf("Vals;    xStep:%d           yStep%d\n", xStep, yStep);
-        printf("Vals;    xLeft:%d           yLeft%d\n", xLft, yLft);    
+        printf("Vals;    xLeft:%d           yLeft%d\n", xToDo, yToDo);
+        
+        printf("Mis Vals;                         decpart:%f usDelay:%d dif:%f yDir:%d xDir%d yToDo:%d xToDo:%d \n",decpart,usDelay,dif,yDir,xDir,yToDo,xToDo);    
             printf("Main For loop\n");
 /*            printf("UsDelay is %d\n", usDelay);
-            printf("xLft is %d\n", xLft);*/
-            for (int *i = &dif; *i > 0; *i--){                  // Inside the x itteration we have the more frequent y itteration
+            printf("xToDo is %d\n", xToDo);*/
+            for (int i = (int)yToDo/xToDo; i > 0; i--){                  // Inside the x itteration we have the more frequent y itteration
                 printf("Fast loop -> ");
                 output_b(HalfStep[yStep&0x7]<<4);
                 yStep+=yDir;
+                yToDo-=yDir;
                 delay_ms(d);
             }
             printf("Slow loop -> ");
@@ -118,16 +126,19 @@ void twoStep(float lALT, float lAZ){                        // ALT == y    AZ ==
             xStep+=xDir;
             //delay_us(usDelay);
         }
-        printf("We made it this far!\n");
+        printf("Done With this loop\n");
         exit;
+
+        //WTF I dont understand any of this code
+
     }
 
-    else if (abs(xLft)>abs(yLft)){                          // IN this case X is higher
-        int dif = xLft/yLft;
-        float mod = xLft%yLft;
-        int usDelay = mod * 1000;
+    else if (abs(xToDo)>abs(yToDo)){                          // IN this case X is higher
+        int dif = xToDo/yToDo;
+        float decpart = xToDo%yToDo;
+        int usDelay = decpart * 1000;
 
-        for (int z = yLft; z > 0; z--){                             // Here we itterate for the value of Y as it lower 
+        for (int z = yToDo; z > 0; z--){                             // Here we itterate for the value of Y as it lower 
             for (int i = dif; i > 0; i--){                  // Inside theYx itteration we have the more frequent X itteration
                 output_b(HalfStep[xStep&0x7]);
                 xStep+=xDir;
@@ -142,7 +153,7 @@ void twoStep(float lALT, float lAZ){                        // ALT == y    AZ ==
 
 
     else 
-        for (int z = abs(yLft); z > 0; z--){
+        for (int z = abs(yToDo); z > 0; z--){
             output_b(HalfStep[yStep&0x7]<<4|HalfStep[xStep&0x7]);
             delay_ms (d);
             xStep ++;
